cffc48bb49e1971f8a3f45c2a7643212ff6dba6d
x13945 <lsk.boy.f@gmail.com>
2019-12-13 16:19:54 +0800

    [android] migrate from python2 to python3

--- python/startActivity.py
+++ python/startActivity.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 import os
 import sys
 
-packageName=str(sys.argv[1])
-activityName=str(sys.argv[2])
+if len(sys.argv) < 3:
+    exit("Please input package name and activity name")
 
-def getDeviceId():
-    devices = []
+packageName = str(sys.argv[1])
+activityName = str(sys.argv[2])
+
+
+def get_device_id():
+    device_list = []
     command = "adb devices -l | sed '1d'| awk '{print $1}'"
     result = os.popen(command)
-    deviceId = result.readline().strip()
-    if deviceId != "":
-        devices.append(deviceId)
+    device_id = result.readline().strip()
+    if device_id != "":
+        device_list.append(device_id)
 
-    while (deviceId != ""):
-        deviceId = result.readline().strip()
-        if deviceId != "":
-            devices.append(deviceId)
-    return devices;
+    while device_id != "":
+        device_id = result.readline().strip()
+        if device_id != "":
+            device_list.append(device_id)
+    return device_list
 
-def startActivity(device, packageName, activityName):
+
+def start_activity(device, package_name, activity_name):
     # print device, packageName
-    print "Got device: " + device
-    command = "adb -s %s shell am start %s/%s"%(device, packageName, activityName)
+    print("Got device: " + device)
+    command = "adb -s %s shell am start %s/%s" % (device, package_name, activity_name)
     os.system(command)
 
-devices = getDeviceId();
-devicesNum = len(devices);
+
+devices = get_device_id()
+devicesNum = len(devices)
 
 if devicesNum < 1:
-    print "Device not found."
+    print("Device not found.")
 elif devicesNum == 1:
     device = devices[0]
-    startActivity(device, packageName, activityName)
+    start_activity(device, packageName, activityName)
 else:
-    print "Please chose a dvice, input the index of the device:"
-    for i in xrange(0, devicesNum):
-        print str(i) + "\t" + devices[i]
-    index = raw_input("")
-    startActivity(devices[int(index)], packageName, activityName)
+    print("Please chose a device, input the index of the device:")
+    for i in range(0, devicesNum):
+        print(str(i) + "\t" + devices[i])
+    index = input("")
+    start_activity(devices[int(index)], packageName, activityName)
